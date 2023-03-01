5d21d0702349c59de0e85cb964755c96d7f1bd4a
J.C Lizarraga <73605425+ityoup@users.noreply.github.com>
2023-03-01 07:36:10 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 const btn = document.getElementById('btn1');
+const border = document.getElementById('card1');
 
 
 btn.addEventListener('click', function handleClick() {
   btn.textContent = 'Comprado';
 });
 
+border.addEventListener('click', function handleClick() {
+  border.style.border = " solid #0000FF";
+});
+
 const btn2 = document.getElementById('btn2');
+const border2 = document.getElementById('card2');
 
 
 btn2.addEventListener('click', function handleClick() {
   btn2.textContent = 'Comprado';
 });
+border2.addEventListener('click', function handleClick() {
+  border2.style.border = " solid #0000FF ";
+});
 
 const btn3 = document.getElementById('btn3');
+const border3 = document.getElementById('card3');
+
 
 
 btn3.addEventListener('click', function handleClick() {
   btn3.textContent = 'Comprado';
+});
+border3.addEventListener('click', function handleClick() {
+  border3.style.border = "solid #0000FF ";
 });
