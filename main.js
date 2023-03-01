94b0fabf8a065257b4697d5c7d9d717c92fbca13
ityoup <lizarraga.juan.1gv@gmail.com>
2023-02-28 19:20:02 -0600

    ya

---
+++ main.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn1');
+
+
+btn.addEventListener('click', function handleClick() {
+  btn.textContent = 'Comprado';
+});
+
+const btn2 = document.getElementById('btn2');
+
+
+btn2.addEventListener('click', function handleClick() {
+  btn2.textContent = 'Comprado';
+});
+
+const btn3 = document.getElementById('btn3');
+
+
+btn3.addEventListener('click', function handleClick() {
+  btn3.textContent = 'Comprado';
+});
